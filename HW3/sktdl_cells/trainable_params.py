2e6cda5454b5a263ea2ee7a16b7b9efa8c4975ab
Serge Kozlukov <newkozlukov@gmail.com>
2019-04-30 13:16:30 +0300

    forgot to stage trainable_params.py

---
+++ HW3/sktdl_cells/trainable_params.py
@@ -0,0 +1,11 @@
+def fixed(*trainable_params):
+    def get(net):
+        return ((n, p) for n, p in net.named_parameters() if n in trainable_params)
+    return get
+
+def headtail(n_head, n_tail):
+    def get(net):
+        pp = list(net.named_parameters())
+        take_first = min(n_head, len(pp) - n_tail)
+        return pp[:take_first] + pp[-n_tail:]
+    return get
