1fe6afd57ea45133c71effc29d6c287140ca88f0
Serge Kozlukov <newkozlukov@gmail.com>
2019-05-04 19:57:42 +0300

    fixup! fixup! fixup! fixup! segmentation: experiment that allows not resetting any part of unet

--- HW3/sktdl_cells/model.py
+++ HW3/sktdl_cells/model.py
@@ -4,7 +4,7 @@ class UnetAsExtractor(nn.Module):
         self.inconv = torch.nn.Conv2d(3, 3, kernel_size=5, padding=2)
         self.unet = UNet(3, 1)
         self.outconv = torch.nn.Conv2d(1, 1, kernel_size=3, padding=1)
-    def forward((self, input):
+    def forward(self, input):
             out = input
             out = self.inconv(out)
             out = self.unet(out)
